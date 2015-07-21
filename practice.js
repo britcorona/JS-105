b63ddde9bb12bab2c55c2dc9d4b9c6dfd7246d31
Brittney Corona <BrittneyLCorona@gmail.com>
2015-07-21 10:00:02 -0500

    Finished

--- practice.js
+++ practice.js
@@ -12,26 +12,30 @@
 */
 
 function toCelsius (input) {
-  celsius(input * 9/5) + 32;
+  return (input * 9 / 5)  + 32;
 }
 
-function toFahrenheit () {
-  fahrenheit(input - 32 / 5 ) + 32;
+function toFahrenheit (input) {
+  return (input - 32 ) * (5 / 9);
 }
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (e) {
-
   // This will actually return an array containing both of the radio buttons
   // so you will need to look at each item in the array and see if the 'checked'
   // property is true (sample code provided)
   var optionChosen = document.getElementsByName("conversion_type");
+  var tempValue = document.getElementById("temperature");
 
+for (var i = 0; i < optionChosen.length; i++) {
   // for loop
-    if (?.checked && ?.value === 'celsius') {
-
+    if (optionChosen[i].checked && optionChosen[i].value === 'celsius') {
+      console.log(toCelsius(tempValue.value));
+    } else if (optionChosen[i].checked && optionChosen[i].value === 'fahrenheit') {
+      console.log(toFahrenheit(tempValue.value) );
     }
+  }
   // end for loop
 }
 
